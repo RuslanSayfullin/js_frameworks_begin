ed50fc3b106aeccaedc3d056c013fa52633a40b6
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-01-25 09:22:37 +0500

    Создали проект на Django.

---
+++ backend/psw.py
@@ -0,0 +1,7 @@
+"""Пароли для системы, внесены в gitignore"""
+secret_key = 'django-insecure-5*ld8q5j@vjg7xdpjrq9+u^*(^o@^4&6*_zuul55*il7m5)&0r'   # секретный ключ приложения
+dbase_psw = '12345'     # Пароль для подключения к БД
+
+# superuser
+# Логин: cryptolis
+# Пароль: MQ6HWuw24LCa
