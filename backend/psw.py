ec9933a4bbfa5e0d47c45185b51fe85ea28c6a65
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-05-20 17:43:53 +0500

    Написание приложения

--- backend/psw.py
+++
@@ -1,7 +0,0 @@
-"""Пароли для системы, внесены в gitignore"""
-secret_key = 'django-insecure-26w#-h7$d@obw2g^r%&!$6cs4s+3^strd^ky8-3wb90*lg8e_4'   # секретный ключ приложения
-dbase_psw = '12345'     # Пароль для подключения к БД
-
-# superuser
-# Логин: cryptolis
-# Пароль: MQ6HWuw24LCa
